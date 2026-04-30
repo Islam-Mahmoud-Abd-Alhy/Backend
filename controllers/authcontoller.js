f11815f1c3f13daf9b1e856f66767b9a06e95f63
Islam-Mahmoud-Abd-Alhy <islam2010ghazy@gmail.com>
2026-04-30 15:28:00 +0300

    Update authcontoller.js

--- controllers/authcontoller.js
+++ controllers/authcontoller.js
@@ -37,7 +37,7 @@ exports.login = async (req, res) => {
         res.cookie('token', token, { 
             httpOnly: true, 
             secure: true, 
-            sameSite: 'Lax', 
+            sameSite: 'none', 
             maxAge: 24 * 60 * 60 * 1000 
         });
 
